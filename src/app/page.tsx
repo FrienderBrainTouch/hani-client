1dfb97bf91a0551bf9800ba6e7a40976da19776e
haewonwon <khcw1029@daum.net>
2025-10-01 13:18:51 +0900

    Feat: 메인 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { HeroSection, ValuesSection, ContentSection } from '@/components/main';
+import {
+  MainBannerSection,
+  CoreValuesSection,
+  ReferencesSection,
+  StrengthsSection,
+  CTASection,
+} from '@/components/main';
 
 export default function Home() {
   return (
     <>
-      <HeroSection />
-      <ValuesSection />
-      <ContentSection />
+      <MainBannerSection />
+      <CoreValuesSection />
+      <ReferencesSection />
+      <StrengthsSection />
+      <div className="max-w-7xl mx-auto px-6 py-20">
+        <CTASection />
+      </div>
     </>
   );
 }
