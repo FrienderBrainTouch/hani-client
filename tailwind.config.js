b30fab82f06d047fb8a7549337f5284f83e77ee9
haewonwon <khcw1029@daum.net>
2025-10-02 10:40:49 +0900

    Feat: Tailwind Custom BreakPoint 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xs: '375px', // 375px
+      sm: '425px', // 425px
+      md: '768px', // 768px
+      lg: '1024px', // 1024px
+      xl: '1440px', // 1440px
+      '2xl': '1920px', // 1920px
+    },
     extend: {
       colors: {
         background: 'var(--background)',
